5c9dcff0fa11a73a0e0c1db5289098698cddd773
siva369 <sxs82900@ucmo.edu>
2018-06-05 16:49:34 -0400

    Initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <tittle> </tittle>
+</head>
+<body>
+<h1> About US</h1>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <tittle> </tittle>
+    </head>
+    <body>
+        <h1> HEllo World</h1>
+    </body>
+</html>
